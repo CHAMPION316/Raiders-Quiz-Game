bd405dad73bcf2f3c01ad4e0c08db1420b44d95b
C316 <royer.seguracalderon@gmail.com>
2021-07-28 17:48:31 +0000

    Complete design for html and css

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let question = document.querySelector('#question');
+let choices = Array.from(document.querySelectorAll('choice.text'));
+let progressText = document.querySelector('#progressText');
+let scoreText = document.querySelector('#score');
+let progressBarFull = document.querySelector('#progressBarFull');
+
+/**
+ * Create Functions for these variables
+ */
