9ad3a81dfcf8b27bf1277bf91291623f5529cf0b
C316 <royer.seguracalderon@gmail.com>
2021-07-28 23:43:29 +0000

    Create quiz questions and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,60 @@ let progressText = document.querySelector('#progressText');
 let scoreText = document.querySelector('#score');
 let progressBarFull = document.querySelector('#progressBarFull');
 
-/**
- * Create Functions for these variables
- */
+let score = 0;
+let theAnswers = true;
+let upcomingQuestions = [];
+let currentQuestion = {};
+let questionCounter = 0;
+
+let questions = [
+    {
+        question: 'What year did the Raiders when their last Super Bowl?',
+        choice1: '2002', 
+        choice2: '1993',
+        choice3: '1983',
+        choice4: '1972',
+    },
+    {
+        question: "What was Jack Tatum's nickname?",
+        choice1: 'Killer Croc',
+        choice2: 'The Assassin',
+        choice3: 'The Jackhammer',
+        choice4: 'Killer Bee',
+    },
+    {
+        question: "Which 'Hall of Fame' Raider wore the number 00",
+        choice1: 'Charles Woodson', 
+        choice2: 'Kenny Stabler',
+        choice3: 'Mike Haynes',
+        choice4: 'Jim Otto',
+    },
+    {
+        question: 'Which Raider became a HollyWood star?',
+        choice1: 'Carl Weathers', 
+        choice2: 'Howie Long',
+        choice3: 'Lyle Alzado',
+        choice4: 'Bo Jackson',
+    },
+    {
+        question: 'Who is Al Davis?',
+        choice1: 'Ex owner', 
+        choice2: 'Current owner',
+        choice3: "QB from the late 70's",
+        choice4: 'Current star Wide Receiver',
+    }
+];
+
+const gamePoints = 500;
+const totalQuestions = 5;
+
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    upcomingQuestions = [...questions];
+    getNewQuestion();
+};
+
+function getNewQuestion() {
+
+};
